99214a40e5695db580b566eb96eab9bbae4e848c
rishidubey0 <rishidubey5676@gmail.com>
2026-09-21 22:29:51 +0530

    img

--- src/services/banner.service.js
+++ src/services/banner.service.js
@@ -4,7 +4,6 @@ const list = () =>
   Banner.findAll({ order: [['sort_order', 'ASC'], ['created_at', 'DESC']] });
 
 const create = async ({ title, subtitle, bannerImage, section, emoji, bgType, deeplink, sortOrder, validTo, isActive }) => {
-  if (!title) throw new Error('Title is required');
   if (!bannerImage) throw new Error('Banner image URL is required');
   return Banner.create({ title, subtitle, bannerImage, section, emoji, bgType, deeplink, sortOrder, validTo, isActive });
 };
